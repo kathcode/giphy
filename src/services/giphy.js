f76387a41fbfd3cfd4a38a48a95f31fe7e954ab0
kathcode <kathmene27@gmail.com>
2021-03-27 13:33:49 -0500

    Added giphy service to get the trending gifs

---
+++ src/services/giphy.js
@@ -0,0 +1,14 @@
+import environment from '../config/environment';
+import axios from 'axios';
+
+export const getTrendingGif = async () => {
+  console.log(environment)
+  const URL = `http://api.giphy.com/v1/gifs/trending?api_key=WnTEYVz8yJSXIH1ZF4mLgRF33Ey4oC1g&limit=10&rating=g&offset=0`;
+  
+  try {
+    const response = await axios(URL);
+    return response.data;
+  } catch (error) {
+    throw Error(error)
+  }
+};
