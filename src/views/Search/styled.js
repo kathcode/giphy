d6935b242225eea2efe83b5c2ae444806a597b03
kathcode <kathmene27@gmail.com>
2021-03-28 10:48:16 -0500

    Update the grid

--- src/views/Search/styled.js
+++ src/views/Search/styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  grid-template-columns: 30% 30% 30%;
+  grid-template-columns: 22% 22% 22% 22%;
   grid-template-rows: auto;
   column-gap: 15px;
   row-gap: 15px;
